b497bb7a81db5de01ba6e88098f19cdcac72d61d
Trin Pinthavirut <t_end_9871@hotmail.com>
2013-05-19 20:38:11 +0700

    link model update

--- grails-app/domain/linklike/Link.groovy
+++ grails-app/domain/linklike/Link.groovy
@@ -3,7 +3,12 @@ package linklike
 class Link {
     String title
     String url
-    
+    Integer votes = 0
+
     static constraints = {
+    }
+
+    void increaseVotes(){
+    	votes += 1
     }
 }
